a021a84ed0844ac642151a57b6cd89a61bb2c867
LucidMach <nukalasuraj7@gmail.com>
2022-08-29 17:41:46 +0530

    req cloud debugging

--- pages/api/pingIP.ts
+++ pages/api/pingIP.ts
@@ -7,6 +7,7 @@ export default async function handler(
   res: NextApiResponse
 ) {
   const { id, ip, port } = req.body;
+  console.log(req.body);
 
   res.status(200).json({
     data: await pingIP(id, ip, port),
@@ -15,9 +16,6 @@ export default async function handler(
 
 export const pingIP = async (id: string, ip: string, port: string) => {
   const { data, error } = await supabase.from("Bot").upsert({ id, ip, port });
-  if (error) {
-    console.log(`for id:${id}@:${ip}:${port}`);
-    console.log(error);
-  }
+  if (error) console.log(error);
   return error ? ":( Oops \n check log in vercel" : "SUCCESS";
 };
