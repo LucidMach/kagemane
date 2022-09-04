eb5962eb482eb3fbaf79c4cc98111af67621ba19
LucidMach <nukalasuraj7@gmail.com>
2022-09-05 00:37:08 +0530

    neumorphic led & button

--- pages/bots/[id].tsx
+++ pages/bots/[id].tsx
@@ -5,28 +5,30 @@ import Links from "../../components/Links";
 import Status from "../../components/Status";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
+import { useState } from "react";
 
 const Bots: NextPage = () => {
+  const [led, setLED] = useState<boolean>(false);
+
   const router = useRouter();
   const { id } = router.query;
 
+  const handleClick = () => {
+    setLED((prev) => !prev);
+  };
+
   const { data, isLoading } = useQuery(["bot", id], async ({ queryKey }) => {
-    // const res = await fetch("/api/getIP", {
-    //   headers: {
-    //     Accept: "application/json",
-    //     "Content-Type": "application/json",
-    //   },
-    //   method: "POST",
-    //   body: JSON.stringify({ id }),
-    // });
-    // console.log(res);
-
-    const res = await axios.post("/api/getIP", {
-      id,
-    });
-
+    const res = await axios.post("/api/getIP", { id });
     return res.data.data;
   });
+
+  const botQueryRender = () => {
+    return isLoading ? (
+      <div>loading...</div>
+    ) : (
+      <div>{`${id} [ws://${data.ip}:${data.port}]`}</div>
+    );
+  };
 
   return (
     <>
@@ -36,12 +38,15 @@ const Bots: NextPage = () => {
       <div className="h-full flex justify-between items-center flex-col bg-shikamaru-green-100 text-shikamaru-green-900">
         <Status />
         <div className="flex flex-col items-center">
-          <strong>bot: {id}</strong>
-          {isLoading ? (
-            <div>loading...</div>
-          ) : (
-            <div>{JSON.stringify(data)}</div>
-          )}
+          <div className="lgt">
+            <div className={`led led--${led}`}></div>
+          </div>
+          <button
+            className="w-[250px] h-[250px] mb-5 bg-shikamaru-green-100 neumorphism rounded-full"
+            onClick={handleClick}
+          ></button>
+          <strong>BotID</strong>
+          {botQueryRender()}
         </div>
         <Links />
       </div>
