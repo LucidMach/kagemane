600e2824ddf65e6127175aae192102d27f28467d
Nukala Suraj <nukalasuraj7@gmail.com>
2022-10-13 18:45:51 +0530

    rPI0 led control

--- hardware/rpi/button/server.js
+++ hardware/rpi/button/server.js
@@ -1,16 +1,28 @@
+const ip = require("ip");
+const {exec} = require("child_process")
 const { WebSocketServer } = require("ws");
 
-const wss = new WebSocketServer({ port: 5000 });
+const pingIP = require("./pingIP");
+
+// updating DB
+const botID = "rPI0"
+pingIP(botID, ip.address())
 
 let ledState = false;
+
+// creating a secure websocketserver
+const wss = new WebSocketServer({ port: 5000 });
 
 wss.on("connection", function connection(ws) {
   console.log("a client connected");
 
   ws.on("message", function message(data) {
-    if (message === "toggle") {
+    const msg = data.toString()
+
+    if (msg === "toggle") {
       ledState = !ledState;
       ws.emit(ledState);
+      exec(`echo ${ledState ? 1 : 0} |sudo tee /sys/class/leds/led0/brightness`)
     }
   });
   ws.on("close", () => {
