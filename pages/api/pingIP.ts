960fc2687b801d5ec8db0b69c13009da7e687bc2
LucidMach <nukalasuraj7@gmail.com>
2022-10-22 16:01:02 +0530

    updating pingIP

--- pages/api/pingIP.ts
+++ pages/api/pingIP.ts
@@ -6,16 +6,24 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const { id, ip } = req.body;
+  const { id, ip, port, secure, endpoint } = req.body;
   console.log(req.body);
 
   res.status(200).json({
-    data: await pingIP(id, ip),
+    data: await pingIP(id, ip, port, secure, endpoint),
   });
 }
 
-export const pingIP = async (id: string, ip: string) => {
-  const { data, error } = await supabase.from("Bot").upsert({ id, ip });
+export const pingIP = async (
+  id: string,
+  ip: string,
+  port?: number,
+  secure?: boolean,
+  endpoint?: string
+) => {
+  const { data, error } = await supabase
+    .from("Bot")
+    .upsert({ id, ip, port, secure, endpoint });
   if (error) console.log(error);
   return error ? ":( Oops \n check log in vercel" : "SUCCESS";
 };
